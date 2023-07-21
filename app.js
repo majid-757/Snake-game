5dc13394a82d1c6e4c8fe40f39152149fad9b762
unknown <majid.asadollahi95@gmail.com>
2023-07-21 12:42:43 +0330

    init the project

---
+++ app.js
@@ -0,0 +1,54 @@
+const cvs = document.getElementById('snake')
+const ctx = cvs.getContext('2d')
+
+// create the unit
+const Box = 32
+
+
+// load images
+const ground = new Image()
+ground.src = './img/ground.png'
+
+
+
+const food = new Image()
+food.src = './img/food.png'
+
+
+let snake = []
+snake[0] = {
+    x: 9*box,
+    y: 10*box,
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
